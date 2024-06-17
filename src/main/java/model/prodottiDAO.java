9e52116be73ec3ed76cb777757deff78c9c25918
AntoDeVita <a.devita41@studenti.unisa.it>
2024-06-17 16:50:38 +0200

    Aggiunta Bean

--- src/main/java/model/prodottiDAO.java
+++ src/main/java/model/prodottiDAO.java
@@ -7,6 +7,6 @@ public class prodottiDAO implements IBeanDAO<prodottoBean>{
 	private static DataSource ds;
 	static {
 	
-		public synchronized selectByPlatform
+		public synchronized selectByPlatform 
 	}
 }
