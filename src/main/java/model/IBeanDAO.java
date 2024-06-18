dc50639fe257614cafccb90455fc0cf6f514c379
AntoDeVita <a.devita41@studenti.unisa.it>
2024-06-18 16:33:58 +0200

    Bean

--- src/main/java/model/IBeanDAO.java
+++ src/main/java/model/IBeanDAO.java
@@ -1,11 +1,12 @@
 package model;
 import java.sql.SQLException;
+import java.util.Collection;
 
 public interface IBeanDAO<T> {
 	
-	public void selectByPlatform(T piattaforma) throws SQLException;
-	public void selectByType(T tipo) throws SQLException;
-	public void selectByGenre(T genere) throws SQLException;
-	public T selectAll(int idProdotti) throws SQLException;
-	
+	public void doSave(T bean) throws SQLException;//Salva il prodotto sul database
+	public boolean doDelete(int idProdotti) throws SQLException;//Cancella un prodotto dal database
+	public T doRetrieveByKey(int idProdotti) throws SQLException;
+	public Collection<T> doRetrieveAll(int idOrdine) throws SQLException;
+	public void doUpdate(T bean) throws SQLException;//Salva le modifiche al prodotto sul database
 }
--- src/main/java/model/prodottiDAO.java
+++ src/main/java/model/prodottiDAO.java
@@ -1,12 +1,72 @@
 package model;
 
-import javax.sql.DataSource;
+import java.sql.*;
+import java.util.Collection;
+import javax.sql.*;
 
 public class prodottiDAO implements IBeanDAO<prodottoBean>{
 	
 	private static DataSource ds;
-	static {
+		final static String TABLE_NAME = "prodotti";
+		
+	@Override
+	public void doSave(prodottoBean bean) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
 	
-		public synchronized selectByPlatform 
+	@Override
+	public boolean doDelete(int idProdotti) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public prodottoBean doRetrieveByKey(int idProdotti) throws SQLException {
+		Connection conn= null;
+		PreparedStatement prep= null;
+		prodottoBean bean= new prodottoBean();
+		String selectSQL= "SELECT * FROM " + prodottiDAO.TABLE_NAME + "WHERE idProdotti= ";
+		try {
+			conn= ds.getConnection();
+			prep= conn.prepareStatement(selectSQL);
+			prep.setInt(1, idProdotti);
+			ResultSet rs= prep.executeQuery();
+			while(rs.next()) {
+				bean.setIdProdotti(rs.getInt("idProdotti"));
+				bean.setNome(rs.getString("nome"));
+				bean.setDescrizione(rs.getString("descrizione"));
+				bean.setCoV(rs.getBoolean("CoV"));
+				bean.setPrezzo(rs.getDouble("prezzo"));
+				bean.setCasaProduttrice(rs.getString("casaProduttrice"));
+				bean.setPiattaforma(rs.getString("piattaforma"));
+				bean.setGenere(rs.getString("genere"));
+				bean.setTipo(rs.getString("tipo"));
+				bean.setDataRilascio(rs.getString("dataRilascio"));
+			}
+		}
+		finally {
+			try {
+				if(prep != null)
+					prep.close();
+			}
+			finally {
+				if(conn != null)
+					conn.close();
+			}
+		}
+		return bean;
+	}
+	
+	@Override
+	public Collection<prodottoBean> doRetrieveAll(int idOrdine) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void doUpdate(prodottoBean bean) throws SQLException {
+		// TODO Auto-generated method stub
+		
 	}
 }
