0bde2cf3287eb305cc7575b65b91e31095f45569
MarcoBattag <m.battaglia13@studenti.unisa.it>
2024-07-12 11:35:48 +0200

    aggiuntaJs

--- src/main/webapp/script/Popup.js
+++ src/main/webapp/script/Popup.js
@@ -1,6 +1,8 @@
-function showPopupElimina() {
+function showPopupElimina(id, nome) {
             document.getElementById('popupElimina').style.display = 'block';
             document.getElementById('overlayElimina').style.display = 'block';
+            document.getElementById('Nome').innerText = nome;
+            document.getElementById('id').value= id;
 	    }
 	    
 	    function hidePopupElimina() {
@@ -8,7 +10,7 @@ function showPopupElimina() {
             document.getElementById('overlayElimina').style.display = 'none';
         }
 	    
-	    function showPopupAggiungi(idProdotto) {
+	    function showPopupAggiungi() {
             document.getElementById('popupAggiungi').style.display = 'block';
             document.getElementById('overlayAggiungi').style.display = 'block';
 	    	
@@ -34,9 +36,21 @@ function showPopupElimina() {
             document.getElementById('tipo').value = tipo;
             document.getElementById('releaseDate').value = releaseDate;
             document.getElementById('qnt').value = qnt;
+            document.getElementById('id2').value= idProdotto;
+
         }
 
         function hidePopupModifica() {
             document.getElementById('popupModifica').style.display = 'none';
             document.getElementById('overlayModifica').style.display = 'none';
+        }
+        
+        function showPopupOrdine(){
+			document.getElementById('popupOrdine').style.display = 'block';
+            document.getElementById('overlayOrdine').style.display = 'block';
+		}
+		
+		function hidePopupOrdine() {
+            document.getElementById('popupOrdine').style.display = 'none';
+            document.getElementById('overlayOrdine').style.display = 'none';
         }
