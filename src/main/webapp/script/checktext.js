785a8996b1cff08cd3a2baa935d6938fad1e6acc
MarcoBattag <m.battaglia13@studenti.unisa.it>
2024-07-12 17:07:30 +0200

    mod

--- src/main/webapp/script/checktext.js
+++ src/main/webapp/script/checktext.js
@@ -7,3 +7,20 @@ function controllaEta(eta){
 	  }
 }
 
+function controllaQuantita(quantita){
+	  if (isNaN(quantita.value) || parseInt(quantita.value)<0)
+	  {
+	    alert('La quantità inserita non è corretta');
+	    quantita.value="";
+	    quantita.focus();
+	  }
+}
+
+function controllaPrezzo(prezzo){
+	  if (isNaN(prezzo.value) || prezzo.value<0)
+	  {
+	    alert('Il prezzo inserito non è corretto');
+	    prezzo.value="";
+	    prezzo.focus();
+	  }
+}
